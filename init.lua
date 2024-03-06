f7548e753ff29c29120aaa10b565e1d2cecbf85a
NguyenLe1605 <lhdnguyen2002@gmail.com>
2024-03-07 06:47:55 +0700

    Intial set up of my neovim

---
+++ init.lua
@@ -0,0 +1,73 @@
+local g = vim.g
+g.mapleader = " "
+g.maplocalleader = " "
+
+local opt = vim.opt
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.rnu = true
+opt.autoindent = true
+opt.smartindent = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.breakindent = true
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = "yes"
+
+-- Decrease update time
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+opt.completeopt = "menuone,noselect"
+
+-- NOTE: You should make sure your terminal supports this
+opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
+})
---
+++ lua/plugins/nonels.lua
@@ -0,0 +1,47 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.rustfmt.with({
+					extra_args = function(params)
+						local Path = require("plenary.path")
+						local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+						if cargo_toml:exists() and cargo_toml:is_file() then
+							for _, line in ipairs(cargo_toml:readlines()) do
+								local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+								if edition then
+									return { "--edition=" .. edition }
+								end
+							end
+						end
+						-- default edition when we don't find `Cargo.toml` or the `edition` in it.
+						return { "--edition=2021" }
+					end,
+				}),
+			},
+			-- For autoformatting on save
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
